d4ef4e665f228c872d3e2112a3141330ba0b0363
Artur Sakhiullin <sakhiullinar@email.com>
2026-07-06 02:58:00 +0500

    push

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.16"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+group = "ru.bradyden"
+version = "0.0.1-SNAPSHOT"
+java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
+repositories { mavenCentral() }
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    runtimeOnly("org.postgresql:postgresql")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("com.h2database:h2")
+}
+tasks.withType<Test> { useJUnitPlatform() }
