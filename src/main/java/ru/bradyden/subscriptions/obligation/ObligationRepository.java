32fe4d43f3c5d3c1b43fafb6842cb502d8793667
Artur Sakhiullin <sakhiullinar@email.com>
2026-07-06 18:34:36 +0500

    push

--- src/main/java/ru/bradyden/subscriptions/obligation/ObligationRepository.java
+++ src/main/java/ru/bradyden/subscriptions/obligation/ObligationRepository.java
@@ -1,5 +1,10 @@
 package ru.bradyden.subscriptions.obligation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 public interface ObligationRepository extends JpaRepository<Obligation, UUID> {
+    List<Obligation> findAllByOrderByDataSledPlatezhaAsc();
+    List<Obligation> findByDataSledPlatezhaBetweenOrderByDataSledPlatezhaAsc(
+        LocalDate nachalo, LocalDate konec);
 }
