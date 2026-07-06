fddf6c22e042d9c4e64d01ce4c10718017203329
Artur Sakhiullin <sakhiullinar@email.com>
2026-07-07 01:51:07 +0500

    wip

--- src/main/java/ru/bradyden/subscriptions/obligation/ObligationService.java
+++ src/main/java/ru/bradyden/subscriptions/obligation/ObligationService.java
@@ -1,5 +1,14 @@
 package ru.bradyden.subscriptions.obligation;
+import ru.bradyden.subscriptions.obligation.dto.CreateObligationRequest;
+import ru.bradyden.subscriptions.obligation.dto.CreateObligationResult;
 import org.springframework.stereotype.Service;
 @Service
 public class ObligationService {
+    private final ObligationRepository repo;
+    public ObligationService(ObligationRepository repo) {
+        this.repo = repo;
+    }
+    public CreateObligationResult sozdat(CreateObligationRequest zapros) {
+        return null;
+    }
 }
