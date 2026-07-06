b46b8866180f2570c0964bd2d6c04af99b566cfb
Artur Sakhiullin <sakhiullinar@email.com>
2026-07-06 05:54:55 +0500

    fix

---
+++ src/main/java/ru/bradyden/subscriptions/obligation/Obligation.java
@@ -0,0 +1,12 @@
+package ru.bradyden.subscriptions.obligation;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Getter @Setter
+public class Obligation {
+    @Id
+    private UUID id;
+}
