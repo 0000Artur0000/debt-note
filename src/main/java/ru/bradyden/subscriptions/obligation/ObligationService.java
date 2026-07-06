a2dc31f2e0d11897037844539d5103302ef196bc
Artur Sakhiullin <sakhiullinar@email.com>
2026-07-07 04:28:31 +0500

    fix

--- src/main/java/ru/bradyden/subscriptions/obligation/ObligationService.java
+++ src/main/java/ru/bradyden/subscriptions/obligation/ObligationService.java
@@ -2,13 +2,28 @@ package ru.bradyden.subscriptions.obligation;
 import ru.bradyden.subscriptions.obligation.dto.CreateObligationRequest;
 import ru.bradyden.subscriptions.obligation.dto.CreateObligationResult;
 import org.springframework.stereotype.Service;
+import java.time.Clock;
+import java.time.LocalDate;
 @Service
 public class ObligationService {
     private final ObligationRepository repo;
-    public ObligationService(ObligationRepository repo) {
+    private final Clock chasy;
+    public ObligationService(ObligationRepository repo, Clock chasy) {
         this.repo = repo;
+        this.chasy = chasy;
     }
     public CreateObligationResult sozdat(CreateObligationRequest zapros) {
-        return null;
+        var segodnya = LocalDate.now(chasy);
+        var o = new Obligation();
+        o.setNazvanie(zapros.nazvanie());
+        o.setSumma(zapros.summa());
+        o.setValuta(zapros.valuta());
+        o.setKategoriya(zapros.kategoriya());
+        o.setPeriodichnost(zapros.periodichnost());
+        o.setDataSledPlatezha(zapros.dataSledPlatezha());
+        o.setStatus(zapros.dataSledPlatezha().isBefore(segodnya)
+            ? Status.EXPIRED : Status.ACTIVE);
+        repo.save(o);
+        return new CreateObligationResult(o, null);
     }
 }
