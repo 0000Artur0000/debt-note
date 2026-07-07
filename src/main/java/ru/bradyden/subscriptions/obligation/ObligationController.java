471d283817ab82f7f9110fe9644a55d1b26f8e8d
Artur Sakhiullin <sakhiullinar@email.com>
2026-07-07 16:31:18 +0500

    update

--- src/main/java/ru/bradyden/subscriptions/obligation/ObligationController.java
+++ src/main/java/ru/bradyden/subscriptions/obligation/ObligationController.java
@@ -1,7 +1,27 @@
 package ru.bradyden.subscriptions.obligation;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import ru.bradyden.subscriptions.obligation.dto.CreateObligationRequest;
+import ru.bradyden.subscriptions.obligation.dto.CreateObligationResult;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 @RestController
 @RequestMapping("/obligations")
 public class ObligationController {
+    private final ObligationService servis;
+    public ObligationController(ObligationService servis) {
+        this.servis = servis;
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreateObligationResult sozdat(
+            @Valid @RequestBody CreateObligationRequest zapros) {
+        return servis.sozdat(zapros);
+    }
+    @GetMapping
+    public List<Obligation> spisok(
+            @RequestParam(required = false) Category kategoriya,
+            @RequestParam(required = false) Status status) {
+        throw new UnsupportedOperationException("pokachto net");
+    }
 }
