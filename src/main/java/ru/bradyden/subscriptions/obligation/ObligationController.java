7ccc6e6c7b971c6a68769d29d0f0d366d276bee2
Artur Sakhiullin <sakhiullinar@email.com>
2026-07-07 07:23:45 +0500

    push

---
+++ src/main/java/ru/bradyden/subscriptions/obligation/ObligationController.java
@@ -0,0 +1,7 @@
+package ru.bradyden.subscriptions.obligation;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+@RequestMapping("/obligations")
+public class ObligationController {
+}
