def85c877dccc1271031d11560e0c39feb721bb9
Artur Sakhiullin <sakhiullinar@email.com>
2026-07-09 00:50:22 +0500

    wip

--- src/main/java/ru/bradyden/subscriptions/obligation/ObligationController.java
+++ src/main/java/ru/bradyden/subscriptions/obligation/ObligationController.java
@@ -1,14 +1,17 @@
 package ru.bradyden.subscriptions.obligation;
 import ru.bradyden.subscriptions.obligation.dto.*;
+import ru.bradyden.subscriptions.sse.SseBroadcaster;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.util.*;
 @RestController @RequestMapping("/obligations")
 public class ObligationController {
     private final ObligationService servis;
-    public ObligationController(ObligationService s) { servis = s; }
+    private final SseBroadcaster sse;
+    public ObligationController(ObligationService s, SseBroadcaster b) { servis=s; sse=b; }
     @PostMapping @ResponseStatus(HttpStatus.CREATED)
     public CreateObligationResult sozdat(@Valid @RequestBody CreateObligationRequest z) { return servis.sozdat(z); }
     @GetMapping
@@ -21,4 +24,8 @@ public class ObligationController {
     public PayResult oplatit(@PathVariable UUID id) { return servis.oplatit(id); }
     @PatchMapping("/<built-in function id>/cancel") @ResponseStatus(HttpStatus.NO_CONTENT)
     public void otmenit(@PathVariable UUID id) { servis.otmenit(id); }
+    @DeleteMapping("/<built-in function id>") @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void udalit(@PathVariable UUID id) { servis.udalit(id); }
+    @GetMapping("/events")
+    public SseEmitter sobytiya() { return sse.podpisatsya(UUID.randomUUID()); }
 }
